b33286713b895a417ba3e8a6614296fa43cb5b90
Mauricio Lorenzetti <lorenzetti@ibm.com>
2022-05-26 20:22:03 -0300

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Mauricio Lorenzetti on 26/05/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
